cc584676869c3aa3da947bed8647dff578117450
alovidal <alo.vidal@duocuc.cl>
2024-09-13 01:52:10 -0300

    modificaciones

--- src/app/recuperar/recuperar.page.ts
+++ src/app/recuperar/recuperar.page.ts
@@ -10,36 +10,7 @@ export class RecuperarPage implements OnInit {
 
   constructor(private router: Router) {}
 
-  user = {
-    username: "",
-  };
 
-  mensaje = "";
-
-  validar() {
-    if (this.user.username.length > 2 && this.user.username.length < 9) {
-      //if (this.user.password.length == 4) {
-        //Funciona
-      this.mensaje = '';
-        /* console.log(this.user.username);
-        let navigationExtras: NavigationExtras = {
-          state: {
-            username: this.user.username,
-            password: this.user.password,
-          },
-        };
-        this.router.navigate(['/inicio'], navigationExtras); */
-      /* } else {
-        console.log('Contraseña incorrecta');
-        this.mensaje = 'Contraseña incorrecta';
-        //No funciona
-      } */
-    } else {
-      console.log('No valide');
-      this.mensaje = 'Correo no cumple (min: 3, max: 8)';
-      //Tampoco funciona
-    }
-  }
   ngOnInit() {
   }
 
